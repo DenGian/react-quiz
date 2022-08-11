144b15847268e52802dbdc19e4f6dee171e5147c
DenGian <mondelaers.ian@gmail.com>
2022-08-11 17:00:47 +0200

    add square 3

---
+++ kahoot-but-better/src/components/Square3.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./css/Square3.css"
+
+export default function Square3(){
+    const square3 = ('♥︎')
+    return(
+        <div className='square3'>{square3}</div>
+    )
+}
