d50c14563ea5862901f38db4fbf8d8aca9542c0d
DenGian <mondelaers.ian@gmail.com>
2022-08-11 16:56:27 +0200

    add header

---
+++ kahoot-but-better/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./css/Header.css"
+
+export default function Header(){
+    const title = ('Too hak Kahoot')
+    return(
+        <header><h1>{title}</h1></header>
+    )
+}
---
+++ kahoot-but-better/src/components/Square1.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Squares(){
+    const square1 = ('1')
+    return(
+        <div className='square1'>{square1}</div>
+    )
+}
