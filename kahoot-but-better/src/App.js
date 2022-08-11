0f4eb29ca7350cf67bb74864e22caf7c89cdcf31
DenGian <mondelaers.ian@gmail.com>
2022-08-11 16:58:18 +0200

    add files

--- kahoot-but-better/src/App.js
+++ kahoot-but-better/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Header from "./components/Header";
+import Fetch from "./components/Fetch";
+import AnswerOptions from "./components/ChooseAnswerTitle";
+import Square1 from "./components/Square1";
+import Square2 from "./components/square2";
+import Square3 from "./components/Square3";
+import Square4 from "./components/square4";
 
 function App() {
     return (
         <div className="App">
-
+            <Header/>
+            <Fetch/>
+            <AnswerOptions/>
+            <Square1/>
+            <Square2/>
+            <Square3/>
+            <Square4/>
         </div>
     );
 }
