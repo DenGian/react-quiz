75af8e40a467dcd858db0ccca00e5650828deb15
DenGian <mondelaers.ian@gmail.com>
2022-08-12 09:55:11 +0200

    add file

---
+++ kahoot-but-better/src/components/square4.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./css/Square4.css"
+
+export default function Square4(){
+    const square4 = ('♦︎')
+    return(
+        <div className='square4'>{square4}</div>
+    )
+}
