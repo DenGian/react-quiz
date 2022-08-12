9bbf3ce70e2a82316d2e9906a8fc9c505a129ce5
DenGian <mondelaers.ian@gmail.com>
2022-08-12 09:54:40 +0200

    add file

---
+++ kahoot-but-better/src/components/square2.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./css/Square2.css"
+
+export default function Square2(){
+    const square2 = ('♣︎')
+    return(
+        <div className='square2'>{square2}</div>
+    )
+}
