8d0737cded58888d687f758f4e5c0d6785f07322
DenGian <mondelaers.ian@gmail.com>
2022-08-14 22:49:52 +0200

    showing first answer

--- kahoot-but-better/src/components/Square1.js
+++ kahoot-but-better/src/components/Square1.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import "./css/Square1.css"
+import AnswerHelper from "./Helper/AnswerHelper";
 
 export default function Square1(){
     const square1 = ('♠︎')
+    const answer1 = AnswerHelper.combination[0]
+    // console.log(data);
+    console.log(AnswerHelper.combination[0])
+
     return(
-        <div className='square1'>{square1}</div>
+        <div className='square1'>
+            {square1}
+            <div>
+                {answer1}
+            </div>
+        </div>
     )
 }
